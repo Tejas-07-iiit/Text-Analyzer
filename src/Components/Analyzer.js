86352b049f5eea988161c8bb892fd6d310136849
Tejas-07-iiit <tejas23106@gmail.com>
2026-01-05 20:00:14 +0530

    mail fix

--- src/Components/Analyzer.js
+++ src/Components/Analyzer.js
@@ -8,7 +8,7 @@ function Analyzer(props) {
     const [Mail , setMail] = useState("");
     
     const [text , setText] = useState("");
-    let mail_array = text.match(/[Link-zA-Z0-9._%+-]+@([Link-zA-Z0-9-]+\.)+[Link-zA-Z]{2,}/g);
+    let mail_array = text.match(/[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}/g);
 
     var i = 0;
 
@@ -75,7 +75,7 @@ function Analyzer(props) {
             newPdf.setLineWidth(0.7); 
             newPdf.line(10, 15, 200, 15);
             newPdf.setFont("helvetica", "normal");
-            let mail_array = text.match(/[Link-zA-Z0-9._%+-]+@([Link-zA-Z0-9-]+\.)+[Link-zA-Z]{2,}/g);
+            let mail_array = text.match(/[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}/g);
 
             let main_mail_do = []
             main_mail_do = mail_array.join('\n')
@@ -115,7 +115,7 @@ function Analyzer(props) {
     
     const setmail_1 = (event) => {
         setMail(event.target.value + '\n')
-    }   
+    }  
     
     const changetoupper = ()=> {
         let newtext = text.toUpperCase();   
