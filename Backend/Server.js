e758b08500c41bf86617a3359c964d9f9b3be4c2
Tejas-07-iiit <tejas23106@gmail.com>
2026-02-23 13:33:19 +0530

    model and api change

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,5 +1,6 @@
 const { default: axios } = require("axios");
 const express = require("express");
+const OpenAI = require("openai")
 
 const cors = require("cors")
 const app = express();
@@ -14,26 +15,42 @@ app.post("/api/genrate" , async (req , res)=> {
     try {
         const {content} = req.body;
 
-        const response = await axios.post("https://openrouter.ai/api/v1/chat/completions" , 
-            {
-                model: "openai/gpt-oss-120b",
-                messages: [
+         const client = new OpenAI({
+            apiKey: process.env.GROQ_API_KEY,
+            baseURL: "https://api.groq.com/openai/v1",
+        });
+
+        const response = await client.chat.completions.create({
+            model: "llama-3.3-70b-versatile",
+            messages: [
                 {
                     role: "user",
                     content: content,
                 }
-                ]
-            },
-            {
-            headers : {
-                Authorization : `Bearer ${process.env.OPEN_API}`,
-                "Content-Type": "application/json",
-            }
-            }
-           
-        )
+            ]
+        });
 
-        res.send(response.data.choices[0].message.content);
+        // const response = await axios.post("https://openrouter.ai/api/v1/chat/completions" , 
+        //     {
+        //         model: "openai/gpt-oss-120b",
+        //         messages: [
+        //         {
+        //             role: "user",
+        //             content: content,
+        //         }
+        //         ]
+        //     },
+        //     {
+        //     headers : {
+        //         Authorization : `Bearer ${process.env.OPEN_API}`,
+        //         "Content-Type": "application/json",
+        //     }
+        //     }
+           
+        // )
+
+        res.send(response.choices[0]?.message?.content || "No summary genrated");
+
     } catch (error) {
         console.log("Something went wrong ..")
         console.log(error.message)
